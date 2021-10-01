baca23ad85e36d098b5bb10e399d6a6a9feb59ca
Philopatire <philofolfol@gmail.com>
2021-10-01 21:02:04 +0200

    Improving Template 4

--- js/index.js
+++ js/index.js
@@ -1,22 +1,22 @@
 // Menu
 {
-	let outerBtn = document.querySelector('nav .container .icon'),
-		innerBtn = document.querySelector('.menu .close'),
+	let openBtn = document.querySelector('nav .container .icon'),
+		closeBtn = document.querySelector('.menu .close'),
 		menu = document.querySelector('.menu'),
 		pageHolder = document.querySelector('.page-holder'),
 		allClicks = document.querySelectorAll('.menu ul li a');
 
-	outerBtn.addEventListener('click', () => {
+	openBtn.addEventListener('click', () => {
 		pageHolder.classList.add('active');
 		menu.style.right = 0;
 	});
 	for (let i = 0; i < allClicks.length; i++) {
 		allClicks[i].addEventListener('click', () => {
-			menu.style.right = -300 + 'px';
 			pageHolder.classList.remove('active');
+			menu.style.right = -300 + 'px';
 		});
 	}
-	innerBtn.addEventListener('click', () => {
+	closeBtn.addEventListener('click', () => {
 		pageHolder.classList.remove('active');
 		menu.style.right = -300 + 'px';
 	});
@@ -25,59 +25,29 @@
 
 // Portfolio
 {
-	let picNums = 3;
+	let picShowed = 3;
+	let posts = document.querySelectorAll('.portfolio .container .all-content .all-port .port');
+	let interval = 3;
 	let btn = document.querySelector('.portfolio .container .all-content button');
-	let picToAdd = {
-		one: {
-			url: 'imgs/portfolio/image-1.jpg',
-			title: 'Project Here',
-			pargraph: `My creative ability is very difficult to measure because 
-      it can manifest in so many surprising and.`
-		},
-		two: {
-			url: 'imgs/portfolio/image-1.jpg',
-			title: 'Project Here',
-			pargraph: `My creative ability is very difficult to measure because 
-      it can manifest in so many surprising and.`
-		},
-		three: {
-			url: 'imgs/portfolio/image-1.jpg',
-			title: 'Project Here',
-			pargraph: `My creative ability is very difficult to measure because 
-      it can manifest in so many surprising and.`
-		},
-		four: {
-			url: 'imgs/portfolio/image-1.jpg',
-			title: 'Project Here',
-			pargraph: `My creative ability is very difficult to measure because 
-      it can manifest in so many surprising and.`
-		}
-	};
 	btn.addEventListener('click', () => {
-		if (Object.keys(picToAdd).length > 0) {
-			let i = 0;
-			for (pic in picToAdd) {
-				addPort(picToAdd[pic].url, picToAdd[pic].title, picToAdd[pic].pargraph);
-				delete picToAdd[pic];
-				if (++i == 3) break;
-			}
-		} else {
-			btn.style.display = 'none';
-		}
+		if (picShowed < posts.length) showMore();
+		else showLess();
 	});
-
-	function addPort(url, title, pargraph) {
-		let parentAdd = document.querySelector('.portfolio .container .all-content .all-port');
-		let add = document.createElement('div');
-		add.classList.add('port');
-		add.innerHTML = `<img src= ${url} alt="Image-${++picNums}" />
-  <div class="caption">
-  <h4>${title}</h4>
-  <p>
-    ${pargraph}
-  </p>`;
-		picNums++;
-		parentAdd.append(add);
+	function showLess() {
+		picShowed = 3;
+		btn.textContent = 'Show Me More';
+		showHide(picShowed);
+	}
+	function showMore() {
+		picShowed += interval;
+		if (picShowed >= posts.length) btn.textContent = 'Show Me Less';
+		showHide(picShowed);
+	}
+	function showHide(n) {
+		posts.forEach((post) => post.classList.remove('active'));
+		for (let i = 0; i < n; i++) {
+			posts[i].classList.add('active');
+		}
 	}
 }
 // End Portfolio
